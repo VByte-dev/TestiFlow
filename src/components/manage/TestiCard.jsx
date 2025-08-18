c4bb21189688490a614bb4c70aae0ce86fbfe659
Vedhesh <vbyte.02@gmail.com>
2025-08-19 01:01:38 +0530

    Day-18

--- src/components/manage/TestiCard.jsx
+++ src/components/manage/TestiCard.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 let TestiCard = (props) => {
 
   // Destructuring props
-  let { data } = props;
+  let { data, deleteTestimonial } = props;
   // console.log(data);
 
   // Destructuring data
@@ -21,12 +21,24 @@ let TestiCard = (props) => {
 
   return (
     <>
-      <div className='bg-white text-black font-space rounded border-2 border-zinc-200 my-2 p-4'>
-        <h1 className='font-bricolage font-bold'>{author_name}</h1>
-        <h2 className='text-zinc-600'>{author_role}</h2>
-        <p className='font-semibold'>{content}</p>
-        <h2 className='text-center'>{ratingStar.map((v, i, a) => <i key={i} className="ri-star-fill text-amber-400"></i>)}</h2>
-
+      <div className='bg-white text-black font-space rounded-md border-2 border-zinc-100 my-2 p-4'>
+        <div className='flex justify-end'>
+          <div className='bg-[#D8C6FF] border-2 border-[#cab1ff] h-10 w-10 flex justify-center items-center text-zinc-700 rounded active:motion-preset-pop motion-duration-75 active:bg-[#cab1ff]' onClick={() => {
+            deleteTestimonial();
+          }}>
+            <i className="ri-delete-bin-line text-lg"></i>
+          </div>
+        </div>
+        <div className='px-2'>
+          <h1 className='font-bricolage font-bold text-lg my-2'>Author: {author_name}</h1>
+          <h2 className='text-zinc-800 font-medium text-base'>Role: {author_role}</h2>
+          <p className='text-zinc-800 font-medium text-base '>Message: {content}</p>
+          <h2 className='flex gap-1 text-lg'>
+            {ratingStar.map((v, i) => (
+              <i key={i} className="ri-star-fill text-amber-400 mt-2"></i>
+            ))}
+          </h2>
+        </div>
       </div>
     </>
   )
--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import supabase from "../lib/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 // Components
 import TestiCard from "../components/manage/testiCard";
+import ProjectName from "../components/ProjectName";
+import Notice from "../components/Notice";
 
 let Manage = (props) => {
   // Destructuring props
@@ -32,13 +35,62 @@ let Manage = (props) => {
     fetchData();
   }, [])
 
+  // Notice message
+  let [noticeMsg, setNoticeMsg] = useState("");
+
+  // Handle idle project
+  let handleIdle = () => {
+    setNoticeMsg("⚠️ Select a project before managing testimonials");
+  }
+
+  // Handle empty testimonial
+  let handleEmpty = () => {
+    setNoticeMsg("⚠️ No testimonials yet. Collect feedback to start managing here.")
+  }
+  useEffect(() => {
+    if (projectName === "Idle" || projectName === "") {
+      handleIdle();
+    }
+    else if (testimonials.length === 0) {
+      handleEmpty();
+    }
+    else {
+      setNoticeMsg("");
+    }
+  }, [projectName, testimonials]);
+
+  // Deleting testimonial
+  let navigateTo = useNavigate();
+
+  let deleteTestimonial = async (v) => {
+    let { error } = await supabase
+      .from('testiflow')
+      .delete() // Delete only the necessary data - fetch the proper card detail from the TestiCard
+      .eq('user_id', user.id);
+    if (error) {
+      console.log(error.message);
+    } else {
+      navigateTo("/app/manage");
+    }
+  }
+
   return (
     <>
-      <div className="my-10 mx-8 lg:mx-80 mt-16">
+      <div className="">
+        {/* Project Name */}
+        <section className="my-8 mx-8 md:my-10 md:mx-10 flex justify-end">
+          <ProjectName projectName={projectName} />
+        </section>
+
+        {/* Notice message */}
+        <section className="mx-8 lg:mx-80 mt-16">
+          <Notice message={noticeMsg} />
+        </section>
+
         {/* Data */}
-        <div className=" rounded p-3">
+        <div className=" rounded p-3 my-10 mx-8 lg:mx-80 mt-16">
           {testimonials.map((v, i, a) => (
-            <TestiCard data={v} />
+            <TestiCard key={i} data={v} deleteTestimonial={deleteTestimonial} />
           ))}
         </div>
       </div>
